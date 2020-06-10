8121899cdd0cbfb0c62397d7f2e1d3e139509839
nahumsa <nahumsaa@gmail.com>
2020-06-10 20:35:09 -0300

    Added unit tests

--- Tests/genesis_block_test.py
+++ Tests/genesis_block_test.py
@@ -1,15 +1,20 @@
+""" To run the unit test python -m unittest Tests/genesis_block_test.py """
+###################################
+####### Passed on 10/06/20 ########
+###################################
+
 import sys
 sys.path.append('../')
 
 import unittest
 from Blockchain import Blockchain
 
-class GenesisBlockTest(unittest.TestCase):
-    def test(self):        
+class TestGenesisBlock(unittest.TestCase):
+    def test(self):                
         self.assertEqual(Blockchain().last_block.index,
-                         '0')
+                         0)
 
 
 
 if __name__ == '__main__':
-    GenesisBlockTest()
+    TestGenesisBlock()

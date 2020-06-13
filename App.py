06811ce26c45c3b5d39c4ecaa98c71e3cd34cc09
nahumsa <nahumsaa@gmail.com>
2020-06-13 14:13:52 -0300

    Added new_transaction endpoint

--- App.py
+++ App.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 
 # Initializing Blockchain object
 blockchain = Blockchain()
+
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    tx_data = request.get_json()
+    required_fields = ["author", "content"]
+
+    for field in required_fields:
+        if not tx.data.get(field):
+            return "Invalid transaction data.", 404
+
+    tx_data["timestamp"] = time.time()
+
+    blockchain.add_new_transaction(tx_data)
+
+    return "Success!", 201
